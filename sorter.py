603106788157a6649fd672ab34fd0dabb7241683
vlc1337 <95694878+vlc1337@users.noreply.github.com>
2023-01-14 16:18:14 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -3,7 +3,13 @@ with open("links.txt", 'r') as link_file:
 
 with open("baza.txt", "r") as baza_file:
     for line in baza_file:
-        url, login, password = line.strip().split(":")
+        parts = line.split(':')
+        login, password = parts[-2], parts[-1]
+
+        parts = parts[:-2]
+        url = ':'.join(parts)
+
+        # print(url, login, password)
         for link in links:
             if link in url:
                 with open(f"{link}.txt", "a") as output_file:
